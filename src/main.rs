dd40bbca8c987c08ca86004d3580c104ea0f0a41
Riken7 <rikenprajapati2005@gmail.com>
2024-04-30 16:13:40 +0530

    --

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use std::io;
+use std::{io, u32};
 fn main(){
     let mut var1 = String::new();
     io::stdin().read_line(&mut var1).expect("Failed to read line");
-    let var1 : u32 = var1.trim().parse().unwrap();
+    // let var1 : u32 = var1.trim().parse().unwrap();
+    let var1 : u32 = var1.trim().parse::<u32>().unwrap();
     let var2 : u32= if var1 < 5  {
         5
     } else {
